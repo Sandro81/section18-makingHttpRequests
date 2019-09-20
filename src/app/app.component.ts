4456a0289771166db4253b72fc877a91f574308b
Sandro81 <sandro.fioravanti81@gmail.com>
2019-09-21 00:46:57 +0100

    section18

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,7 @@ import {PostModel} from './post.model';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  loadedPosts = [];
+  loadedPosts: PostModel[] = [];
 
   constructor(private http: HttpClient) {}
 
@@ -57,6 +57,7 @@ export class AppComponent implements OnInit {
       }))
       .subscribe(
         posts => {
+          this.loadedPosts = posts;
           console.log(posts);
         });
   }
