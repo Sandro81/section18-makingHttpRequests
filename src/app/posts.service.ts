bf854c13bb4e9134a1ba25cda66ae90b18139216
Sandro81 <sandro.fioravanti81@gmail.com>
2019-09-23 14:48:07 +0100

    section18

--- src/app/posts.service.ts
+++ src/app/posts.service.ts
@@ -1,8 +1,8 @@
 import {Injectable} from '@angular/core';
 import {HttpClient, HttpClientModule} from '@angular/common/http';
 import {PostModel} from './post.model';
-import {map} from 'rxjs/operators';
-import {Subject} from 'rxjs';
+import {map, catchError} from 'rxjs/operators';
+import {Subject, throwError} from 'rxjs';
 
 @Injectable({providedIn: 'root'})
 export class PostsService {
@@ -38,6 +38,10 @@ export class PostsService {
             }
           }
           return postsArray;
+        }),
+        catchError(erroeRes => {
+          // Send to analytics server
+          return throwError(erroeRes);
         })
       );
   }
