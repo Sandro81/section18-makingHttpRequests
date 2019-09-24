4bf5340437d954a8be00e16d8b7f5830cfafbb48
Sandro81 <sandro.fioravanti81@gmail.com>
2019-09-24 21:11:44 +0100

    section18

--- src/app/posts.service.ts
+++ src/app/posts.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core';
-import {HttpClient, HttpClientModule, HttpHeaders} from '@angular/common/http';
+import {HttpClient, HttpClientModule, HttpHeaders, HttpParams} from '@angular/common/http';
 import {PostModel} from './post.model';
 import {map, catchError} from 'rxjs/operators';
 import {Subject, throwError} from 'rxjs';
@@ -27,11 +27,15 @@ export class PostsService {
   }
 
   fetchPosts() {
+    let searchParams = new HttpParams();
+    searchParams = searchParams.append('print', 'pretty');
+    searchParams = searchParams.append('custom', 'key');
     return this.http
       .get<{ [key: string]: PostModel }>(
         'https://section18angularcourse.firebaseio.com/posts.json',
         {
-          headers: new HttpHeaders({'Custom-Header': 'Hello'})
+          headers: new HttpHeaders({'Custom-Header': 'Hello'}),
+          params: searchParams
         }
       )
       .pipe(map((responseData: { [key: string]: PostModel }) => {
